cf63d2e4700499700a9a916b9f418b87d2e6e025
taiki ogasawara <ogataiki@gmail.com>
2015-05-30 16:27:50 +0900

    Initial Commit

---
+++ BuyMEMO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuyMEMO
+//
+//  Created by 小笠原 大樹 on 2015/05/30.
+//  Copyright (c) 2015年 ogataiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
