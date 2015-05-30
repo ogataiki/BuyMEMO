3cbc4d4e4721def6f494e37990958213090ba0e5
taiki ogasawara <ogataiki@gmail.com>
2015-05-30 18:25:47 +0900

    とりあえずテーブルビューがwatch側で表示される

--- BuyMEMO WatchKit Extension/InterfaceController.swift
+++ BuyMEMO WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,23 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var table: WKInterfaceTable!
+    
+    var rows = ["太郎😍", "花子🐽", "サンタクロース🎅", "あ", "i", "おすあふぇいwjふぁえw"]
+
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
         // Configure interface objects here.
+        
+        // tableの設定
+        table.setNumberOfRows(rows.count, withRowType: "TableRowController");
+        
+        // tableRowのラベルにtableItemsの要素を表示
+        for (index, item) in enumerate(rows){
+            let controller = table.rowControllerAtIndex(index) as! TableRowController
+            controller.BuyItem.setTitle(item);
+        }
     }
 
     override func willActivate() {
@@ -26,6 +39,10 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+
+    override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
+        self.pushControllerWithName("DetailController", context: self.rows[rowIndex])
     }
 
 }
---
+++ BuyMEMO WatchKit Extension/TableRowController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import WatchKit
+
+class TableRowController: NSObject {
+    
+    @IBOutlet weak var BuyItem: WKInterfaceSwitch!
+    
+    @IBAction func TapSwitch(value: Bool) {
+        // スイッチ切り替えた
+        
+    }
+}
