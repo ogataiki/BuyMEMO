22e26f2d513ea57e040d9bff44b4da255dbfcc42
taiki ogasawara <ogataiki@gmail.com>
2015-05-30 16:30:35 +0900

    startup

---
+++ BuyMEMO WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  BuyMEMO WatchKit Extension
+//
+//  Created by 小笠原 大樹 on 2015/05/30.
+//  Copyright (c) 2015年 ogataiki. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
